56c266302d3842d54c8b2bb69a11ef2ebf9a0f89
linwt <37769342+linwt@users.noreply.github.com>
2018-12-20 15:12:40 +0800

    Add files via upload

--- leetcode/925.长键按入.py
+++ leetcode/925.长键按入.py
@@ -1,31 +1,11 @@
-# 方法一：遍历name为基础
+# 在两个字符串长度范围内，用两个指针指向两个字符串的首部，若两个元素相同则两指针各前进一步，否则typed的指针前进一步
 class Solution(object):
     def isLongPressedName(self, name, typed):
         i = j = 0
-        # 遍历name字符
-        while i < len(name):
-            # name中字符未完，typed字符已经结束
-            if j >= len(typed):
-                return False
-            elif name[i] == typed[j]:
+        while i < len(name) and j < len(typed):
+            if name[i] == typed[j]:
                 i += 1
-                j += 1
-            elif i >= 1 and typed[j] == typed[j-1]:
                 j += 1
             else:
-                return False
-        return True
-
-# 方法二：遍历typed为基础
-class Solution(object):
-    def isLongPressedName(self, name, typed):
-        i, name_len = 0, len(name)
-        # 遍历typed字符
-        for j in range(len(typed)):
-            # name字符未结束
-            if i < name_len and name[i] == typed[j]:
-                i += 1
-            elif j == 0 or typed[j] != typed[j-1]:
-                return False
-        # typed字符遍历完后，判断name字符是否也结束
-        return i == name_len
+                j += 1
+        return i == len(name)
