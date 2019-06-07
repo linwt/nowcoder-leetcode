2b3a80b91c6acb4eb83e8b6bac641294b0c0d852
linwt <37769342+linwt@users.noreply.github.com>
2019-06-07 17:06:43 +0800

    Update 064.最小路径和.py

--- leetcode/064.最小路径和.py
+++ leetcode/064.最小路径和.py
@@ -31,3 +31,22 @@ class Solution2(object):
                 else:
                     grid[i][j] += min(grid[i-1][j], grid[i][j-1])
         return grid[-1][-1]
+
+    
+# 动态规划经典写法
+class Solution(object):
+    def minPathSum(self, grid):
+        m, n = len(grid), len(grid[0])
+        dp = [[0]*n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        # 初始化首行首列
+        for i in range(1, m):
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+        for j in range(1, n):
+            dp[0][j] = dp[0][j-1] + grid[0][j]
+        # 遍历每个位置
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+                
+        return dp[-1][-1]
