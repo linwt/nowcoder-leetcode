cdc331848553fca93df441f439b58120f266f949
Boy ln <37769342+linwt@users.noreply.github.com>
2020-05-03 17:40:07 +0800

    Update 406.根据身高重建队列.py

--- leetcode/406.根据身高重建队列.py
+++ leetcode/406.根据身高重建队列.py
@@ -1,4 +1,4 @@
-# 先按升高降序排序，身高相同则按k升序排序。再以k为索引插入排序
+# 先按身高降序排序，身高相同则按k升序排序。再以k为索引插入排序
 class Solution(object):
     def reconstructQueue(self, people):
         people.sort(key=lambda x:(-x[0], x[1]))
