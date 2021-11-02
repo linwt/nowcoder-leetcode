72837e5fb22daa5eea8811de3202a1bef99f8afa
linwt <1076538498@qq.com>
2021-11-02 23:05:48 +0800

    更新

---
+++ leetcode_Java/Solution0094.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// 二叉树的中序遍历
+class Solution0094 {
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return list;
+    }
+}
