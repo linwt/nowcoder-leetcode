46aa01def5e32a55472b2dcf75c809af61b80975
linwt <1076538498@qq.com>
2022-04-06 07:12:22 +0800

    704.二分查找

---
+++ leetcode_Java/Solution0704.java
@@ -0,0 +1,20 @@
+// 704. 二分查找
+
+
+// 进阶题“33.搜索旋转排序数组”
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
