61070a47333cd99faad84e901fd79d23f7907137
linwt <1076538498@qq.com>
2022-04-28 20:54:54 +0800

    98.验证二叉搜索树

---
+++ leetcode_Java/Solution0098.java
@@ -0,0 +1,85 @@
+// 98. 验证二叉搜索树
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+class Solution {
+    private long pre = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        return isValidBST(root.right);
+    }
+}
+
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
+    }
+
+    private boolean isValid(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) {
+            return true;
+        }
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        return isValid(root.left, min, root) && isValid(root.right, root, max);
+    }
+}
+
+
+/*
+“94. 二叉树的中序遍历”，中序遍历存入列表，再遍历判断是否升序
+ */
+class Solution {
+    public List<Integer> list = new ArrayList<>();
+
+    public boolean isValidBST(TreeNode root) {
+        inorderTraversal(root);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) <= list.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+        return list;
+    }
+}
