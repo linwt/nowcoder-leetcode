1a59291b2718249d34eb365f9a86976da17109ce
Boy ln <37769342+linwt@users.noreply.github.com>
2024-03-27 09:34:42 +0800

    Update Solution0152.java

--- leetcode_Java/Solution0152.java
+++ leetcode_Java/Solution0152.java
@@ -9,7 +9,7 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int res = nums[0], imax = 1, imin = 1;
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0) {
                 int temp = imax;
                 imax = imin;
